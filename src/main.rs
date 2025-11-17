40cff95953600dd45e9d83fccbfcd46a372895b5
akshq96 <akshitraj19@gmail.com>
2025-11-17 11:28:57 +0530

    updatde

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,24 @@
-
+//! A simple program to organize files in a folder into different categories.
+//! 
+//! The program will organize files into the following categories:
+//! - Images
+//! - Videos
+//! - Music
+//! - Documents
+//! - Archives
+//! - Applications
+//! 
+//! Usage: cargo run [folder_path]
+//! Example: cargo run ./Downloads
+//! 
+//! The program will print the files that are moved to the console.
+//! 
+//! The program will exit with code 1 if the directory does not exist or is not a directory.
+//! 
 
 use std::{fs,io,path::Path, env};
 
-fn main()-> io::Result<()> {
+fn main()-> io::Result<()> { 
     // Get folder path from command line argument or use default
     let folder_path = env::args().nth(1).unwrap_or_else(|| "./Downloads".to_string());
     
